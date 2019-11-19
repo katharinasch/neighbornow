60dc4409119ef0210c7f3db10d797205e32e3b2f
Katharina Schreiber <katharinaschreiber@Katharinas-MacBook-Pro.local>
2019-11-19 14:18:40 -0600

    Almost finished profiles, redirect with a bug

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,9 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+	protected
+
+	  def after_sign_up_path_for(resource)
+	    redirect_to new_user_profile_path(current_user.id) # Or :prefix_to_your_route
+	  end
+  # Override the action you want here.
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	devise_for :user, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+	devise_for :user, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
 	resources :users do
    		resources :profiles
 	end
