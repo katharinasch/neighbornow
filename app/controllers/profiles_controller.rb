c10987df7ce4cdf204154a1fd7ef56b87a8ad1fb
Katharina Schreiber <katharinaschreiber@Katharinas-MacBook-Pro.local>
2019-11-20 00:43:00 -0600

    Login, Logout, Profiles work

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,10 +4,10 @@ class ProfilesController < ApplicationController
 
   # GET /profiles
   # GET /profiles.json
-  def index
-    @profiles = Profile.all
+  #def index
+    #@profiles = Profile.all
     #current_user.build_profile
-  end
+  #end
 
   # GET /profiles/1
   # GET /profiles/1.json
@@ -31,7 +31,7 @@ class ProfilesController < ApplicationController
 
     respond_to do |format|
       if @profile.save
-        format.html { redirect_to profile_params, notice: 'Profile was successfully created.' }
+        format.html { redirect_to user_profile_path(current_user.id, current_user.profile.id), notice: 'Profile was successfully created.' }
         format.json { render :show, status: :created, location: @profile }
       else
         format.html { render :new }
@@ -45,7 +45,7 @@ class ProfilesController < ApplicationController
   def update
     respond_to do |format|
       if @profile.update(profile_params)
-        format.html { redirect_to profile_url(@profile), notice: 'Profile was successfully updated.' }
+        format.html { redirect_to user_profile_path(current_user.id, current_user.profile.id), notice: 'Profile was successfully updated.' }
         format.json { render :show, status: :ok, location: @profile }
       else
         format.html { render :edit }
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,6 @@ class Profile < ApplicationRecord
 	belongs_to :user
 	validates :about, presence: true
 	validates :user_id, presence: true
+	has_one_attached :avatar
 	# has_one_attached :avatar
 end
